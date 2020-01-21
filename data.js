0660e017a37bc23a2c6261f614fe3a21e5446045
ankitamehra <60144591+ankitamehra@users.noreply.github.com>
2020-01-21 21:57:15 +0530

    Update data.js

--- data.js
+++ data.js
@@ -32,23 +32,23 @@ var resumeData = {
                 duration : 'July 2017-Present',
                 allprojects:[
                 {
-                    project : 'Cleartrip  Hotels Content',
-                    content : `Auto Search Suggestions for 60 cities (India) - Lay of the Land feature for Hotels product - Descriptions cross-check forwarded by the outsource agency - SEO friendly content creations for hotels (PAN India)- 
+                    project : 'Content for Hotels, Social Media & Product Enhancement',
+                    content : `Auto Search Suggestions for 60 cities (India) - Lay of the Land feature for Hotels product - Descriptions cross-check forwarded by the outsource agency - SEO friendly content creation for hotels (PAN India)- 
                     Product and Engineering Team coordination for resolving content and product related issues - Business development content-related requests management on a daily basis- Ad-hoc projects execution and management - 
-                    HR and PR Team coordination as and when required - Timely feedback to the Content as well as Product Team (Mobile App and Website both) - Content trackers management on a regular basis`
+                    HR and PR Team coordination as and when required - Timely feedback to the Content as well as Product Team (Mobile App and Website both) - Content trackers management on a regular basis.`
                 }]
             },
             {
                 designation : 'Manager - Content & PR',
                 companyName: 'MYP GROUP OF COMPANIES - GURGAON',
-                duration: 'April 2017-Juno 2017',
+                duration: 'April 2017-June 2017',
                 allprojects:[
                     {
-                        project : 'Content Management and PR',
-                        content : `Created core strategy for the brand Designed tho logo -  
-                                    Wrote and edited SE0 friendly articles for the blog - Provided feedback to the outsourco agency for a commercial vvebsite creation 
-                                    -Coordinated with the HR Department and company's heod for the successful implementation of company's guidelines - 
-                                    Worked on social media for the brand promotion `
+                        project : 'Content Management, Social Media and PR',
+                        content : `Created core strategy for the brand - Designed the logo -  
+                                    Wrote and edited SE0 friendly articles for the blog - Provided feedback to the outsource agency for a commercial website creation 
+                                    -Coordinated with the HR Department and company's head for the successful implementation of company's guidelines - 
+                                    Worked on social media for brand promotion.`
                     }
                 ]
             },
@@ -58,12 +58,27 @@ var resumeData = {
                 duration: 'May 2016-March 2017',
                 allprojects:[
                     {
-                        project : 'Content, Design and SEO',
-                        content : `Finalized the website's layout and wrote/edited coherent SEO friendly content for the website 
+                        project : 'Content, Design and Social Media',
+                        content : `Finalized the website's layout - Wrote/edited coherent SEO friendly content for the website - 
                                     - Updated SE0 friendly content on Wordpress - Represented the organization by generating blog articles 
-                                    - Managed the team of Bloggors 
+                                    - Managed the team of Bloggers 
                                     - Managed daily content updates on social media 
                                     - Coordinated with the Technical Team to fix the gaps 
+                                  `
+                    }]
+                },
+            {
+                designation : 'Senior Content Executive',
+                companyName: ' CLEARTRIP PRIVATE LlMITED - GURGAON',
+                duration: 'July 2012-May 2016',
+                allprojects:[
+                    {
+                        project : 'Content and Product (Hotels)',
+                        content : `Coordinated with the in-house team for the introduction of a new overview template - 
+                                    - Executed location clean-up of 200+ cities - Prepared Zero Tolerance Report for 60+ cities 
+                                    - Coordinated with the Production Support Team for better presentation of Inclusions, Property Type, Facilities and Amenities on the website - Guided the new members on maintaining the content quality at different levels - Worked on 1 International City - Dubai along with the agency for coherent website content - Managed content related requests from the BD Team - Hotels (PAN India) - Created SEO-friendly content for hotels - Managed the work of an outsource agency on a daily basis and provided timely feedback 
+                                    
+                             
                                   `
                     }]
                 }
@@ -73,55 +88,55 @@ var resumeData = {
         heading: 'Education',
         qualifications:[
             {
-                institute: 'Manipal Institute of Technology (Udupi)',
-                duration: '2010 - 2014',
-                degree: 'Bachelor of Engineering (BE)',
-                specialization: 'Computer Science and Engineering'
+                institute: 'Miranda House, University of Delhi',
+                duration: '2009 - 2011',
+                degree: 'Master of Arts',
+                specialization: 'English Literature'
             }
         ]
     },
     'skills' : {
-        heading : 'Front End Skills',
+        heading : 'Proficiencies',
         topics : [
             {
-                name : 'Front-end Web Design',
-                rating: 3
-            },
-            {
-                name : 'Backbone JS',
-                rating: 3
-            },
-            {
-                name : 'MongoDB Aggregations',
-                rating: 2
-            },
-            {
-                name : 'mustacheJS',
-                rating: 3
-            },
-            {
-                name : 'lodash',
+                name : 'SEO',
                 rating: 4
             },
             {
-                name : 'p5.js',
+                name : 'WordPress',
+                rating: 4
+            },
+            {
+                name : 'Blogspot',
+                rating: 4
+            },
+            {
+                name : 'MS Word',
+                rating: 4
+            },
+            {
+                name : 'KDP - Amazon',
+                rating: 4
+            },
+            {
+                name : 'MS Office',
                 rating: 3
             },
             {
-                name : 'Ionic + AngularJS',
+                name : 'asana',
                 rating: 3
             },
             {
-                name : 'ReactJS',
-                rating: 2
-            },
-            {
-                name : 'Vanilla Javascript',
+                name : 'Atlassian Jira',
                 rating: 3
             },
             {
-                name : 'CSS',
+                name : 'CleverTap',
                 rating: 3
+            },
+            {
+                name : 'Canva',
+                rating: 4
             }
 
         ]
