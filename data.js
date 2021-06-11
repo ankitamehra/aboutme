b1f423766e7435ebefc37a2520ee4f2fdf1854c0
ankitamehra <60144591+ankitamehra@users.noreply.github.com>
2021-06-11 10:51:07 +0530

    city update

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ var resumeData = {
         name : 'Ankita Mehra',
         email : 'ankita.mehra1988@gmail.com',
         designation : 'Deputy Manager - Communications',
-        current City : 'Varanasi',
+        currentCity : 'Varanasi, India',
         imgurl: 'https://images-na.ssl-images-amazon.com/images/I/914sMCV6VZL._US230_.jpg',
         metaTags : [
             {
